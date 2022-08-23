a220a7e9e7c363500677f7d1e1dcba3e4d1875ce
Mohdzahid0786 <mohmmadzahid1999@gmail.com>
2022-08-23 21:34:22 +0530

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 
 import './App.css';
-import Login from './Components/Login/Login';
-import  Homepage  from './Components/Homepage/Homepage';
-import Register from './Components/Register/Register';
-import News from './Components/News';
-
+import {useGetAllPostQuery} from './Services/Post';
 function App() {
+  const responseInfo=useGetAllPostQuery()
+
+  console.log("Response Information:",responseInfo)
+  console.log("Data:",responseInfo.data)
+
+  if(responseInfo.isLoading)return <div>Loading....</div>
+  if(responseInfo.isError)return <div>An error is occured{responseInfo.error.error}</div>
+  
   return (
-    <>
-     <div className='App'>
-     {/* <Login /> */}
-     {/* <Homepage /> */}
-     {/* <Register /> */}
-     <News  />
-     
-     </div>
-    </>
+
+    <div className='App'>
+      <h1>Redux Toolkit(Get All Data)</h1>
+      {
+        responseInfo.data.map((post)=>
+        <div>
+          <h3>{post.id}{post.title}{post.body}</h3>
+          
+        </div>
+        )
+      }
+    </div>
+
   );
 }
 
